300072d2e9f34b57421155f476d03d6b4d80a06a
Hexley <timo.mauerer@icloud.com>
2026-04-15 21:57:53 +0200

    Added C callable `allocate` function
    Uses the new @c attribute so its only available in Swift >= 6.3

---
+++ Sources/Allocators/MemoryArena/C Interop.swift
@@ -0,0 +1,17 @@
+/// Include the following forward declaration in your C code:
+/// ```c
+/// void* arena_allocate(void* allocator, intptr_t bytecount, intptr_t alignment);
+/// ```
+#if swift(>=6.3)
+@c func arena_allocate(
+    _ allocator: OpaquePointer,
+    _ bytecount: Int,
+    _ alignment: Int,
+) -> UnsafeMutableRawPointer
+{
+    return UnsafeMutablePointer<UnsafeMemoryArena>
+        .init(allocator)
+        .pointee
+        .allocate(byteCount: bytecount, alignment: alignment)
+}
+#endif
